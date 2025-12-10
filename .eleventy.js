6400be32d841c72df7e84cf4b1d3baf4d98b4e87
Brad McLaughlin <brad@clevercouch.com>
2025-12-10 12:30:39 -0600

    trigger build

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,16 @@
 module.exports = function(eleventyConfig) {
-  // Copy CSS straight through to public/css
+  // Passthrough for CSS
   eleventyConfig.addPassthroughCopy({ "src/css": "css" });
 
-  // Copy images to public/img
+  // Passthrough for images
   eleventyConfig.addPassthroughCopy({ "src/img": "img" });
-
-  // Copy any .jpg file
-  eleventyConfig.addPassthroughCopy("**/*.jpg");
 
   return {
     dir: {
       input: "src",
       includes: "_includes",
       layouts: "_includes/layouts",
-      output: "public"   // CHANGE THIS from dist -> public
+      output: "public"  // Cloudflare Pages automatically serves 'public/'
     }
   };
 };
